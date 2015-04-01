27f1652b481faf8d1fb43e250989af853be8abb0
ihlee001 <iainhlee@gmail.com>
2015-03-31 17:28:03 -0700

    Uploading broncoapps project

---
+++ app/src/main/java/com/example/iain/broncoapps/GMaps.java
@@ -0,0 +1,16 @@
+package com.example.iain.broncoapps;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Iain on 3/24/2015.
+ */
+public class GMaps extends FragmentActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
